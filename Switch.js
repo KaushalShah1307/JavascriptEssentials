e3791ff5a068021e9de2927fc5e59dec0f97340f
Kaushal Shah <KaushalShah1307@gmail.com>
2016-04-30 21:15:40 -0400

    Lets Learn Switch in JS

---
+++ Switch.js
@@ -0,0 +1,21 @@
+/**
+ * Created by kshah on 4/30/16.
+ */
+
+var fName = "Kaushal";
+
+switch (fName) {
+
+    case "Jainam":
+        console.log("Nay! This is Jainam, Kaushal`s li`l brother");
+        break;
+
+    case "Roger":
+        console.log("Hey! Roger long time no see!");
+        break;
+
+    default:
+        console.log("None of these men is Kaushal!");
+        break;
+
+}
